3a6fb262f1bd79da33ff98bf069b40d562b2c1af
KGSanchez <soportekda@gmail.com>
2024-04-19 16:40:23 -0400

    Adding Login to Auth

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -56,4 +56,50 @@ router.post('/register', async (req, res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+    try {
+        // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+
+        // Task 2: Access MongoDB `users` collection
+        const collection = db.collection('users');
+
+        // Task 3: Check for user credentials in database
+        const { email, password } = req.body;
+        const theUser = await collection.findOne({email: email});
+
+        // Task 4: Check if the password matches the encrypted password and send appropriate message on mismatch
+        if(theUser) {
+            let result = await bcryptjs.compare(password, theUser.password);
+            if(!result){
+                logger.error('Password do not match');
+                return res.json(404).json({ error: 'Wrong password' });
+            }
+
+            // Task 5: Fetch user details from a database
+            const { userName, userEmail } = theUser;
+
+            let payload = {
+                user: {
+                    id: theUser._id.toString()
+                }
+            };
+
+            // Task 6: Create JWT authentication if passwords match with user._id as payload
+            const authToken = jwt.sign(payload, JWT_SECRET);
+
+            res.json({authToken, userName, userEmail });
+        } else {
+            // Task 7: Send appropriate message if the user is not found
+            logger.error('User not found');
+            return res.json(404).json({ error: 'User not found.' });
+        }
+
+    } catch (e) {
+        logger.error(e)
+         return res.status(500).send('Internal server error');
+
+    }
+});
+
 module.exports = router;
